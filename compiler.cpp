d754a293566b1f10b6a93e9b3f91f632da136830
Stefan <me@stefanmada.com>
2024-09-16 23:01:31 -0600

    Can read file

--- compiler.cpp
+++ compiler.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-int main() {
-  cout << "Hello world\n";
+string readFile(string fileName) {
+  ifstream fileStream(fileName);
+
+  if(!fileStream.is_open()) {
+    cerr << "Unable to open file " << fileName << endl;
+    exit(-1);
+  }
+
+  string code;
+  string line;
+
+  while(getline(fileStream, line))
+    code += line + "\n";
+
+  return code;
+}
+
+int main(int argc, char** argv) {
+  if(argc != 2) {
+    cerr << "Need exactly one file argument to interpret" << endl;
+    exit(-1);
+  }
+
+  string code = readFile(argv[1]);
+
+  cout << code << endl;
 }
