4e0c5bf5c410fa5b3ab3c6ecad773f27ff561b0b
Stefan <me@stefanmada.com>
2024-10-14 23:12:51 -0600

    Added timing script

---
+++ doTimings.py
@@ -0,0 +1,34 @@
+from os import listdir
+import subprocess
+from os.path import isfile, join
+import pandas as pd
+import time
+
+onlyfiles = [f for f in listdir("benches") if isfile(join("benches", f))]
+cliOptions = ["", "--vectorize-mem-scans false", "--simplify-loops false", "--vectorize-mem-scans false --simplify-loops false"]
+
+df = pd.DataFrame(data=onlyfiles)
+
+for idx, option in enumerate(cliOptions):
+  timings = []
+  
+  for file in onlyfiles:
+    subprocess.run(
+      ['./compiler.out', f"benches/{file}", "-o", "scriptasm.s"] + option.split()
+    )
+    subprocess.run(
+      ['clang', "scriptasm.s", "-o", "scriptasm.out"]
+    )
+    
+    time_start = time.time()
+    result = subprocess.run(
+      ['time', './scriptasm.out'],
+      capture_output=subprocess.DEVNULL,
+      universal_newlines=True,
+      encoding="cp437"
+    )
+    time_elapsed = time.time() - time_start
+    timings.append(time_elapsed)
+  df.insert(idx + 1, option, timings)
+
+print(df)
